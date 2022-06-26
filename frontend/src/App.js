8563f03ba59546d83b9e82cf00008e192b7fd371
jengoetz <jennifer.goetz@koho.ca>
2022-06-26 00:37:00 -0400

    add router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,24 @@
 import "./App.css";
 import { Buffer } from "buffer";
 import Form from "./components/form/Form";
+import Dashboard from "./components/dashboard/Dashboard";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import Profile from "./pages/Homepage";
 
 function App() {
   window.Buffer = Buffer;
   return (
     <div>
-      {/* <Dashboard /> */}
-      <Form />
-      {/* <BaseOptionChartStyle /> */}
+      <Dashboard />
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/grants" element={<Form />} />
+          <Route path="/form" element={<Form />} />
+          <Route path="/profile" element={<Profile />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
--- frontend/src/components/dashboard/Dashboard.js
+++ frontend/src/components/dashboard/Dashboard.js
@@ -16,31 +16,6 @@ import Paper from "@mui/material/Paper";
 import Link from "@mui/material/Link";
 import WalletConnect from "../WalletConnect";
 import UnlockButton from "../UnlockButton";
-import * as React from 'react';
-import { styled, createTheme, ThemeProvider } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-import MuiDrawer from '@mui/material/Drawer';
-import Box from '@mui/material/Box';
-import MuiAppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import List from '@mui/material/List';
-import Typography from '@mui/material/Typography';
-import Divider from '@mui/material/Divider';
-import IconButton from '@mui/material/IconButton';
-import Badge from '@mui/material/Badge';
-import Container from '@mui/material/Container';
-import Grid from '@mui/material/Grid';
-import Paper from '@mui/material/Paper';
-import Link from '@mui/material/Link';
-import WalletConnect from '../WalletConnect';
-// import { Charts } from './BaseOptionChartStyle';
-// import MenuIcon from '@mui/icons-material/Menu';
-// import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-// import NotificationsIcon from '@mui/icons-material/Notifications';
-// import { mainListItems, secondaryListItems } from './listItems';
-// import Chart from './Chart';
-// import Deposits from './Deposits';
-// import Orders from './Orders';
 
 function Copyright(props) {
   return (
@@ -143,7 +118,7 @@ function DashboardContent() {
               noWrap
               sx={{ flexGrow: 1 }}
             >
-              Dashboard
+              Granted
             </Typography>
             <IconButton color="inherit">
               <Badge badgeContent={4} color="secondary">
@@ -321,8 +296,6 @@ function DashboardContent() {
                   style={{ marginRight: "4rem" }}
                 >
                   {/* <Chart /> */}
-                  {/* <Charts /> */}
-
                 </Paper>
               </Grid>
               {/* Recent Deposits */}
